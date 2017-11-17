9542c515febdb77ef67dbdf4952cdd8cb0b26f16
Gosuke Miyashita <gosukenator@gmail.com>
2017-11-17 19:16:35 +0900

    Fix

--- 04_object/mruby-object/src/object.c
+++ 04_object/mruby-object/src/object.c
@@ -2,17 +2,18 @@
 #include "mruby.h"
 #include "mruby/data.h"
 
-typedef struct point_S point_t;
-extern point_t *point_new(int, int);
-extern point_t *point_add(point_t *, point_t *);
-extern point_t *point_print(point_t *);
+typedef int point;
 
-const static struct mrb_data_type mrb_point_type = { "Point", mrb_free };
+extern point *point_new(int, int);
+extern point *point_add(point *, point *);
+extern point *point_print(point *);
 
-static mrb_value point_new_(mrb_state *mrb, mrb_value self)
+struct mrb_data_type mrb_point_type = { "Point", mrb_free };
+
+mrb_value new(mrb_state *mrb, mrb_value self)
 {
     mrb_int x, y;
-    point_t *p;
+    point *p;
 
     mrb_get_args(mrb, "ii", &x, &y);
     p = point_new(x, y);
@@ -22,9 +23,9 @@ static mrb_value point_new_(mrb_state *mrb, mrb_value self)
     return self;
 }
 
-static mrb_value point_add_(mrb_state *mrb, mrb_value self)
+mrb_value add(mrb_state *mrb, mrb_value self)
 {
-    point_t *p1, *p2;
+    point *p1, *p2;
     mrb_value other;
 
     mrb_get_args(mrb, "o", &other);
@@ -36,9 +37,9 @@ static mrb_value point_add_(mrb_state *mrb, mrb_value self)
     return self;
 }
 
-static mrb_value point_print_(mrb_state *mrb, mrb_value self)
+mrb_value point_print_(mrb_state *mrb, mrb_value self)
 {
-    point_t *p;
+    point *p;
     p = DATA_PTR(self);
     point_print(p);
     return self;
@@ -46,9 +47,10 @@ static mrb_value point_print_(mrb_state *mrb, mrb_value self)
 
 void mrb_mruby_object_gem_init(mrb_state *mrb)
 {   
-    struct RClass *p = mrb_define_class(mrb, "Point", mrb->object_class);
-    mrb_define_method(mrb, p, "initialize", point_new_, MRB_ARGS_REQ(2));
-    mrb_define_method(mrb, p, "add", point_add_, MRB_ARGS_REQ(1));
+    struct RClass *p =
+        mrb_define_class(mrb, "Point", mrb->object_class);
+    mrb_define_method(mrb, p, "initialize", new, MRB_ARGS_REQ(2));
+    mrb_define_method(mrb, p, "add", add, MRB_ARGS_REQ(1));
     mrb_define_method(mrb, p, "print", point_print_, MRB_ARGS_NONE());
 }
 
